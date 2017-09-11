9a0175ce263af923ba814c66ae62b7d5e5ba1ca0
Kevin Williams <williamsk@caltronics.net>
2017-09-11 15:07:08 -0700

    Master commit

--- GitDemo/GitDemo/Program.cs
+++ GitDemo/GitDemo/Program.cs
@@ -10,25 +10,7 @@ namespace GitDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This program will add numbers together");
-            Console.WriteLine("Enter a number");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter another number");
-            int b = Convert.ToInt32(Console.ReadLine());
-            Add(a, b);
-            Average(a, b);
-        }
-
-        public static void Add(int a, int b)
-        {
-            Console.WriteLine($"The sum of the two numbers is {a + b}");
-        }
-
-        public static void Average(int a, int b)
-        {
-            Console.Write("\nThe average of the two numbers is ");
-            Console.WriteLine((a + b) / 2);
-            Console.Read();
+            
         }
     }
 }
